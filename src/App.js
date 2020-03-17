8c0bba69612d7b9c3e6f80331728b74dadf349d7
Sam Hoffman <samdhoffman@gmail.com>
2020-03-16 20:54:04 -0400

    Add dependencies, remove deleted files, and update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
+import { Container, Row, Col, Nav, Navbar } from 'react-bootstrap'
+
 function App() {
+  // const [lightData, setLightData] = useState(null);
+  // useEffect(() => {
+  //   fetch('/hi').then(res => res.json()).then(data => {
+  //     setLightData(data)
+  //   });
+  // }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg" sticky="top">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Container>
+        <Row>
+          <Col xs={12} sm={12} md={5} lg={4} className="stats-container">
+            stats to go here
+          </Col>
+          <Col xs={12} sm={12} md={7} lg={8}>
+            chart slider to go here
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
