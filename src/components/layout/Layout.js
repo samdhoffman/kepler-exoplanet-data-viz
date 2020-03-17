729ed39b745aac18ef70962cad73b808f024c156
Sam Hoffman <samdhoffman@gmail.com>
2020-03-16 21:56:58 -0400

    Update navbar

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -8,14 +8,8 @@ const Layout = (props) => {
     <div className="Layout">
       <nav className="Navbar">
         <Navbar bg="light" expand="lg" sticky="top">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Kepler Exoplanet Data Visualization</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
         </Navbar>
       </nav>
       <main className="content">
